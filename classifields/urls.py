1eaf8f03658821a7f73f88176760877023871a48
Umar Asghar <umar.asghar@arbisoft.com>
2020-11-30 01:31:11 +0500

    add url for static files

--- classifields/urls.py
+++ classifields/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 
@@ -20,4 +21,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include('django_classified.urls', namespace='django_classified')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
